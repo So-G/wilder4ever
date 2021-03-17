1146e3d48303f3bfc4c13a3b00d49dde91671e1f
Solene <solene.gerard@gmail.com>
2021-03-17 10:58:33 +0100

    new files

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log(chalk.yellow("Doriane"));
+console.log(chalk.red("Edouard"));
+console.log(chalk.green("Joris"));
+console.log(chalk.blue("Solène"));
+
+/**c */
